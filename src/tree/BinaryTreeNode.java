1cfe576356fd14c6a59295b1d579c5bebce1190c
deeksha-kashyap <deekshakashyap01@gmail.com>
2019-04-13 18:26:18 +0530

    insert a constructor with data variable

--- src/tree/BinaryTreeNode.java
+++ src/tree/BinaryTreeNode.java
@@ -3,4 +3,10 @@ package tree;
 public class BinaryTreeNode {
     public int data;
     public BinaryTreeNode left,right;
+
+    public BinaryTreeNode(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
 }
