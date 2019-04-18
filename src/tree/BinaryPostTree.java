683d179e0b9496337d056c482a593dc22e66199f
deeksha-kashyap <deekshakashyap01@gmail.com>
2019-04-18 22:19:43 +0530

    add a getter and setter of the variables

--- src/tree/BinaryPostTree.java
+++ src/tree/BinaryPostTree.java
@@ -7,4 +7,28 @@ public class BinaryPostTree {
     private int data;
     private PostNode left;
     private PostNode right;
-}
+
+      public int getData() {
+          return data;
+      }
+
+      public void setData(int data) {
+          this.data = data;
+      }
+
+      public PostNode getLeft() {
+          return left;
+      }
+
+      public void setLeft(PostNode left) {
+          this.left = left;
+      }
+
+      public PostNode getRight() {
+          return right;
+      }
+
+      public void setRight(PostNode right) {
+          this.right = right;
+      }
+  }
