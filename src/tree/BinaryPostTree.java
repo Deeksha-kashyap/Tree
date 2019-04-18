90a94560eeee7c026b7222c6abef1cb16545b4b1
deeksha-kashyap <deekshakashyap01@gmail.com>
2019-04-18 22:41:57 +0530

    get the value to the left and then set the value at the right

--- src/tree/BinaryPostTree.java
+++ src/tree/BinaryPostTree.java
@@ -21,6 +21,7 @@ public class BinaryPostTree {
      root.setLeft(obj.insert(4));
      root.setRight(obj.insert(8));
      root.getLeft().setLeft(obj.insert(6));
+     root.getLeft().setRight(obj.insert(5));
     }
 }
   class PostNode{
