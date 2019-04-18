a163f25778e8dfb8b5079f7471a6eeafd21edad6
deeksha-kashyap <deekshakashyap01@gmail.com>
2019-04-18 23:01:09 +0530

    enter the getter and setter of data, left, right variables

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -7,4 +7,28 @@ class Node1{
   private int data;
   private Node1 left;
   private Node1 right;
+
+  public int getData() {
+    return data;
+  }
+
+  public void setData(int data) {
+    this.data = data;
+  }
+
+  public Node1 getLeft() {
+    return left;
+  }
+
+  public void setLeft(Node1 left) {
+    this.left = left;
+  }
+
+  public Node1 getRight() {
+    return right;
+  }
+
+  public void setRight(Node1 right) {
+    this.right = right;
+  }
 }
